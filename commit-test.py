2a3c6bb89776b8fe42551c9156f97ec7ab9d1523
Alberto Tomás <118726061+albertotm55@users.noreply.github.com>
2025-01-16 10:55:29 +0100

    Añadiendo descripción de un vaso de plástico

---
+++ commit-test.py
@@ -0,0 +1,3 @@
+# Descripción de un vaso de plástico
+
+Un vaso de plástico es un recipiente desechable, a menudo utilizado para bebidas frías. Está hecho de materiales como el polipropileno o el poliestireno, lo que lo hace ligero y económico. Los vasos de plástico son populares en eventos al aire libre, fiestas y otros lugares donde se necesita una solución rápida y conveniente para servir bebidas.
